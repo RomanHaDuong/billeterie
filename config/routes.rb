8f01c2787050e94287b701fc3aa605cd7cd3376f
Roman HaDuong <roman.haduong@gmail.com>
2024-12-25 19:22:28 +0700

    removed home controller / switched it with pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   get 'home/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  root "home#index"
+  root "pages#home"
 
   resources :offres
   resources :bookings
