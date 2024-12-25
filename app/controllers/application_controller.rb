36d30cab75b4cf339117f6ed1c39a1da20225852
Roman HaDuong <roman.haduong@gmail.com>
2024-12-25 18:36:06 +0700

    new fournisseur form implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user_fournisseur
+
+  private
+
+  def set_current_user_fournisseur
+    @current_user_fournisseur = Fournisseur.find_by(user_id: current_user.id) if user_signed_in?
+  end
 end
