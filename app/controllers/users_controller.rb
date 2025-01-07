1277256611e0b322b1655275eb3a662f50e0001c
Roman HaDuong <roman.haduong@gmail.com>
2025-01-07 14:17:05 +0100

    updated layout styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @bookings = current_user.bookings.includes(:offre).order(created_at: :desc)
-    @offres = Favori.where(user_id: current_user.id).map(&:offre)
+    @offres = @bookings.map(&:offre).sort_by(&:date_prevue)
   end
 end
