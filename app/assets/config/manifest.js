035cbfffc4c377585a7802070ba86b03c26b31fd
RomanHaDuong <roman.haduong@gmail.com>
2025-11-26 18:14:39 +0700

    Remove problematic vendor/javascript link from asset manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../javascripts .js
-//= link_tree ../../../vendor/javascript .js
 //= link popper.js
 //= link bootstrap.min.js
 //= link rails_admin.css
