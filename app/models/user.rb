1b1471626a1ec372de82512a5e9d9cdb933df6d8
RomanHaDuong <roman.haduong@gmail.com>
2025-11-27 18:31:36 +0700

    Fix syntax error in User model - properly comment out OmniAuth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,16 +12,16 @@ class User < ApplicationRecord
 
   # Disabled OmniAuth for now
   # def self.from_google_oauth2(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name
-
-      if auth.info.image.present?
-        downloaded_image = URI.open(auth.info.image)
-        user.image.attach(io: downloaded_image, filename: "profile.jpg")
-      end
-
+  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.email = auth.info.email
+  #     user.password = Devise.friendly_token[0, 20]
+  #     user.name = auth.info.name
+  #
+  #     if auth.info.image.present?
+  #       downloaded_image = URI.open(auth.info.image)
+  #       user.image.attach(io: downloaded_image, filename: "profile.jpg")
+  #     end
+  #
   #     user.google_token = auth.credentials.token
   #     user.google_refresh_token = auth.credentials.refresh_token
   #   end
