db6f11f5259f039b7d688d56a03ac955569975dc
RomanHaDuong <roman.haduong@gmail.com>
2025-11-27 18:31:00 +0700

    Disable OmniAuth temporarily - keep config but comment out

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :validatable
+         # :omniauthable, omniauth_providers: [:google_oauth2]
 
-  def self.from_google_oauth2(auth)
+  # Disabled OmniAuth for now
+  # def self.from_google_oauth2(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
@@ -21,8 +22,8 @@ class User < ApplicationRecord
         user.image.attach(io: downloaded_image, filename: "profile.jpg")
       end
 
-      user.google_token = auth.credentials.token
-      user.google_refresh_token = auth.credentials.refresh_token
-    end
-  end
+  #     user.google_token = auth.credentials.token
+  #     user.google_refresh_token = auth.credentials.refresh_token
+  #   end
+  # end
 end
