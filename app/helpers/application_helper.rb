77309c9153e285c901da17b948617326e2001a9e
RomanHaDuong <roman.haduong@gmail.com>
2026-01-11 10:04:08 +0100

    UI improvements: larger logo and menu on desktop, updated time format to H:MM, centered participation page content, removed registration buttons from offres index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def format_time_french(datetime)
+    return '' unless datetime
+    
+    if datetime.min.zero?
+      datetime.strftime("%H:00")
+    else
+      datetime.strftime("%H:%M")
+    end
+  end
 end
